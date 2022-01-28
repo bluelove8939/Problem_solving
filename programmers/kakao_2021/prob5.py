faa690ee69791dbc4ec61bc96a06349f5e88aa6a
Seongwook Kim <bluelove8939@naver.com>
2022-01-28 12:09:40 +0900

    ADD FILES

--- programmers/kakao_2021/prob5.py
+++ programmers/kakao_2021/prob5.py
@@ -1,27 +1,67 @@
-class BinTreeNode:
-    def __init__(self, value=0):
-        self.value = 0
-        self.distance = 0
-        self.parent_sheep = None
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
         self.lnk = []
 
-
-    def nodeinit(self, parent_sheep=None, distance=0):
-        self.parent_sheep = parent_sheep
-        self.distance = distance
-        for nd in self.lnk:
-            if self.value == 0: nd.nodeinit(self, 0)
-            else: nd.nodeinit(parent_sheep, distance+1)
+    def makelink(self, nd):
+        if nd not in self.lnk:
+            self.lnk.append(nd)
 
 
-    def dfs(self, sheep, wolf):
-        pass
+def current_cluster(nd):  # the node 'nd' needs to be a sheep node
+    if nd.value == 1:
+        return 0
+
+    sheep_nodes, wolf_nodes = [nd], []
+    sheep_cnt = 1
+
+    while len(sheep_nodes) != 0:
+        nd = sheep_nodes.pop(-1)
+
+        for child in nd.lnk:
+            if child.value == 0:
+                sheep_nodes.append(child)
+                sheep_cnt += 1
+            else:
+                wolf_nodes.append(child)
+
+    return sheep_cnt, wolf_nodes
 
 
 def solution(info, edges):
-    nodes = [BinTreeNode(value=val) for val in info]
-    for ed in edges: nodes[ed[0]].lnk.append(nodes[ed[1]])
-    answer = nodes[0].dfs(sheep=0, wolf=0)
+    # Preprocessing inputs
+    nodes = [TreeNode(val) for val in info]
+    for parent, child in edges:
+        nodes[parent].makelink(nodes[child])
+
+    sheep_nodes, wolf_nodes = [], []  # nodes to be visited
+    sheep_cnt, wolf_cnt, distance = 0, 0, 0
+    sheep_nodes.append(nodes[0])
+
+    # Repeat the overall task until there aren't any node to visit
+    while len(sheep_nodes) != 0 or len(wolf_nodes) != 0:
+        # Search for current clusters
+        while len(sheep_nodes) != 0:
+            nd = sheep_nodes.pop(-1)
+            tmp_sheep_cnt, tmp_wolf_nodes = current_cluster(nd)
+            sheep_cnt += tmp_sheep_cnt
+            wolf_cnt += distance
+            wolf_nodes += tmp_wolf_nodes
+
+        # Search for next clusters
+        while len(sheep_nodes) == 0 and len(wolf_nodes) != 0:
+            distance += 1
+            tmp_wolf_nodes = []
+
+            while len(wolf_nodes) != 0:
+                nd = wolf_nodes.pop(-1)
+                for child in nd.lnk:
+                    if child.value == 0: sheep_nodes.append(child)
+                    else: tmp_wolf_nodes.append(child)
+
+            wolf_nodes = tmp_wolf_nodes
+
+    answer = sheep_cnt
     return answer
 
 
