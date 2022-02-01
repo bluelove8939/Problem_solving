4cf26ae568584261397224f0be8071eb121e4b9e
Seongwook Kim <bluelove8939@naver.com>
2022-02-01 19:18:04 +0900

    ADD FILES

--- programmers/kakao_2021/prob6.py
+++ programmers/kakao_2021/prob6.py
@@ -1,13 +1,33 @@
 def solution(board, skill):
     answer = 0  # the number of preserved buildings
+    width, height = len(board[0]), len(board)
+    diff = [[0 for _ in range(width+1)] for _ in range(height+1)]
 
-    # sort the skill array
-    skill = sorted(skill, key=lambda x:(x[1], x[2], x[3], x[4]))
+    # generate imos
+    for tp, r1, c1, r2, c2, deg in skill:
+        diff[r1][c1] += deg * (-1 if tp == 1 else 1)
+        diff[r1][c2+1] += deg * (1 if tp == 1 else -1)
+        diff[r2+1][c1] += deg * (1 if tp == 1 else -1)
+        diff[r2+1][c2+1] += deg * (-1 if tp == 1 else 1)
 
+    # calculate accumulative sum by row axis
+    for cidx in range(height):
+        asum = 0
+        for ridx in range(width):
+            diff[ridx][cidx] += asum
+            asum = diff[ridx][cidx]
+
+    # calculate accumulate sum by column axis
+    for ridx in range(width):
+        asum = 0
+        for cidx in range(height):
+            diff[ridx][cidx] += asum
+            asum = diff[ridx][cidx]
+
+    # calculate the number of buildings preserved
     for ridx, row in enumerate(board):
         for cidx, value in enumerate(row):
-
-            # check whether the building is preserved
+            value += diff[ridx][cidx]
             if value > 0: answer += 1
 
     return answer
